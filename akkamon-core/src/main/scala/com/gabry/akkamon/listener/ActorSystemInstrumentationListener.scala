528659d92018f545eeebb04c9230b4afb4ebb761
gabrywu <wu_shao_jie@qq.com>
2018-08-28 17:22:36 +0800

    修改listeners代码，用Queue实现消息的缓存，并增加缓存长度限制

--- akkamon-core/src/main/scala/com/gabry/akkamon/listener/ActorSystemInstrumentationListener.scala
+++ akkamon-core/src/main/scala/com/gabry/akkamon/listener/ActorSystemInstrumentationListener.scala
@@ -1,41 +1,58 @@
 package com.gabry.akkamon.listener
 
-import java.util.concurrent.atomic.AtomicInteger
-
 import com.gabry.akkamon.protocol.InjectMessage
+import com.typesafe.config.ConfigFactory
+
+import scala.annotation.tailrec
+import scala.collection.immutable.{Queue => ImmutableQuque}
 
 /**
   * Created by gabry on 2018/8/27 14:55
   */
 object ActorSystemInstrumentationListeners{
+  private val config = ConfigFactory.load().getConfig("listeners")
+
   private var listeners = List.empty[ActorSystemInstrumentationListener]
   // 在系统启动过程中，可能还没有listener启动，所以先缓存startupMessageLength长度的消息，
   // 并发送给第一个listener
-  private val startupMessageLength = 1000
-  private val startupMessage = new Array[InjectMessage](startupMessageLength)
-  private val bufferPos = new AtomicInteger(0)
+  private val stashCapacity = config.getInt("stash.capacity")
 
+  private var stashedMessages = ImmutableQuque.empty[InjectMessage]
+
+  private def stash(message:InjectMessage):Unit = {
+    if( stashedMessages.length > stashCapacity ){
+      stashedMessages = stashedMessages.dropRight(1)
+    }
+    stashedMessages = stashedMessages.enqueue(message)
+  }
+  private def unStash[T](messageProcessor: InjectMessage => T ):Unit = {
+    @tailrec
+    def unStashAll(messages:ImmutableQuque[InjectMessage]):Unit = {
+      if(messages.nonEmpty){
+        val (message,tailMessages) = messages.dequeue
+        messageProcessor(message)
+        unStashAll(tailMessages)
+      }
+    }
+    unStashAll(stashedMessages)
+
+  }
   def register(newListener:ActorSystemInstrumentationListener):Unit = {
     if(!newListener.isInstanceOf[UnListenedListener]){
       listeners = newListener :: listeners
+      unStash{notifyMessage}
     }
-    if( startupMessage.nonEmpty && bufferPos.get < startupMessageLength )
-      0 until bufferPos.get foreach { idx =>
-        val message = startupMessage(idx)
-        println(s"message = $message idx = $idx")
-        listeners.foreach(listener => if(listener.filter(message)) listener.onMessage(message))
-      }
-
   }
   def unRegister(oldListener:ActorSystemInstrumentationListener):Unit = {
     listeners = listeners.filter( _ != oldListener )
   }
   def notifyMessage(message:InjectMessage):InjectMessage = {
-    assert(message!=null)
-    if( listeners.isEmpty ){
-      startupMessage(bufferPos.getAndIncrement()%startupMessageLength) = message
-    }else{
-      listeners.foreach(listener => if(listener.filter(message)) listener.onMessage(message))
+    if(message != null){
+      if( listeners.isEmpty ){
+        stash(message)
+      }else{
+        listeners.foreach(listener => if(listener.filter(message)) listener.onMessage(message))
+      }
     }
     message
   }
